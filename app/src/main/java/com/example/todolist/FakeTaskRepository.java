1bd5e21413f67c633ff36ceaf23277b5448b80bb
sergi <sergyporto100@hotmail.com>
2025-04-09 20:22:55 +0200

    fix: FakeTaskRepository.java

--- app/src/main/java/com/example/todolist/FakeTaskRepository.java
+++ app/src/main/java/com/example/todolist/FakeTaskRepository.java
@@ -1,5 +1,6 @@
 package com.example.todolist;
 
+
 import java.util.List;
 import java.util.ArrayList;
 
--- app/src/test/java/com/example/todolist/FakeTaskRepositoryTest.java
+++ app/src/test/java/com/example/todolist/FakeTaskRepositoryTest.java
@@ -2,8 +2,8 @@ package com.example.todolist;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.List;
+
 
 import static org.junit.Assert.*;
 
